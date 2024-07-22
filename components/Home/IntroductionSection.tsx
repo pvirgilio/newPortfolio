9e20b76c146976c535fa73eb8c86b04736edf696
Pedro Virgilio <pvvirg16@gmail.com>
2024-07-22 02:45:29 -0300

    ajuste de resolução no componente

--- components/Home/IntroductionSection.tsx
+++ components/Home/IntroductionSection.tsx
@@ -7,13 +7,13 @@ import { Image, Link } from "@nextui-org/react";
 
 export function IntroductionSection() {
   return (
-    <section className="relative w-full bg-gradient-to-r min-h-[95vh] from-[#AD2B49] to-transparent flex flex-col items-center">
+    <section className="relative w-full bg-gradient-to-r min-h-[91vh] from-[#AD2B49] to-transparent flex flex-col items-center">
       <article className=" container flex flex-col items-center pt-4 z-10">
         <div className="flex justify-center flex-wrap gap-1 text-3xl font-bold text-center">
           <h1 className="">Olá, meu nome é</h1>
           <h2 className="whitespace-nowrap ">Pedro Virgilio</h2>
         </div>
-        <div className="flex justify-center flex-wrap gap-1 text-xl font-medium text-center">
+        <div className="mt-2 flex justify-center flex-wrap gap-1 text-xl font-medium text-center">
           <h2>Desenvolvedor</h2>
           <h2 className="whitespace-nowrap">Front-End</h2>
         </div>
@@ -34,13 +34,14 @@ export function IntroductionSection() {
         <Icons />
       </article>
 
-      <div className="absolute bottom-0 z-0 right-0 nm:right-1/2 nm:translate-x-1/2">
+      <div className="absolute bottom-0 z-0 right-0 sm:right-1/2 sm:translate-x-1/2">
         <Image
-          isBlurred
           alt="Pedro Virgilio"
           className="w-auto h-auto rounded-lg object-cover"
           disableSkeleton={true}
+          height={450}
           src="/me.png"
+          width={450}
         />
       </div>
     </section>
