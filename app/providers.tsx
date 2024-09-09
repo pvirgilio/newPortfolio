2083f8fd0afea95e45eded649a6b34f13c4945c2
Pedro Virgilio <pvvirg16@gmail.com>
2024-09-09 13:56:24 -0300

    fix: Correção providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -11,12 +11,12 @@ export interface ProvidersProps {
   themeProps?: ThemeProviderProps;
 }
 
-export function Providers({ children, themeProps }: ProvidersProps) {
+export function Providers({ children }: ProvidersProps) {
   const router = useRouter();
 
   return (
     <NextUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      <NextThemesProvider>{children}</NextThemesProvider>
     </NextUIProvider>
   );
 }
